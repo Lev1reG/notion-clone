66c05e03ab8249a4eeb93d8217f3c56e9f07fc1e
Lev1reG <derentanaphan@mail.ugm.ac.id>
2025-07-10 12:57:26 +0700

    feat: dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/ThemeProviders";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -32,8 +33,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="dotion-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
